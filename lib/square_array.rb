4b4917ce25dc40d237f03844c600707cf235c8fd
Alfred Wang <alfredwang0707@gmail.com>
2020-09-30 21:16:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
 new_array = []
-  while index < array.length {
+  while index < array.length do
     new_array << array[index] * array[index]
     index += 1
-  }
-
-  new_array
+  end
 end
